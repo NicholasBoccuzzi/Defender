cadea3f288ec94b3ee994b409af0651217d42587
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-06-14 17:47:20 -0400

    successfully spawns multiple types of enemies based on the asset path

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEditor;
 
 public class EnemySpawner : MonoBehaviour {
-	public GameObject enemy;
+	private GameObject enemy;
 	private int x = 0;
 	
 
@@ -13,12 +13,27 @@ public class EnemySpawner : MonoBehaviour {
 		// transform is attached to EnemyFormation because this script is attached to EnemyFormation;
 		// Want to make sure that the enemy is instantiated inside of the EnemyFormation
 		// Get 'enemy' out of Instantiate by setting 'GameObject enemy' (declared above) = the instantiation below, but must instantiate 'as' a GameObject (seen below).
-		enemy = Instantiate(AssetDatabase.LoadAssetAtPath("Assets/Entities/StartingEnemy.prefab", typeof(GameObject)), new Vector3(0,0,0), Quaternion.identity) as GameObject;
-		enemy.transform.parent = transform;
+		
+		// for each being used to equip each position with an instance of an enemy.
+		int count = 1;
+		foreach( Transform child in transform) {
+			if (count == 1) {
+				generateEnemy("Assets/Entities/King.prefab", child);
+				count += 1;
+			} else {
+				generateEnemy("Assets/Entities/Pawn.prefab", child);
+				count += 1;				
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void generateEnemy(string path, Transform child) {
+		enemy = Instantiate(AssetDatabase.LoadAssetAtPath(path, typeof(GameObject)), child.transform.position, Quaternion.identity) as GameObject;
+		enemy.transform.parent = child;
 	}
 }
