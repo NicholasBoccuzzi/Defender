38cb23b41719a6d8c192ca5fc957686d8aa8e835
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-06-14 12:22:31 -0400

    added in enemy and added in enemy spawner. enemy is spawned as child of enemyFormation parent

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemy;
+	private int x = 0;
+	
+
+	// Use this for initialization
+	void Start () {
+		// transform is attached to EnemyFormation because this script is attached to EnemyFormation;
+		// Want to make sure that the enemy is instantiated inside of the EnemyFormation
+		// Get 'enemy' out of Instantiate by setting 'GameObject enemy' (declared above) = the instantiation below, but must instantiate 'as' a GameObject (seen below).
+		enemy = Instantiate(AssetDatabase.LoadAssetAtPath("Assets/Entities/StartingEnemy.prefab", typeof(GameObject)), new Vector3(0,0,0), Quaternion.identity) as GameObject;
+		enemy.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -4,27 +4,45 @@ using UnityEngine;
 
 public class ShipMovement : MonoBehaviour {
 
-	public float speed = 15.0f;
+	private float speed = 20.0f;
+	private float padding = .2f;
+	float xmin;
+	float xmax;
 
 	// Use this for initialization
 	void Start () {
+
+		// finding the right distance to each side of the camera.
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector2 rightmost = Camera.main.ViewportToWorldPoint(new Vector3 (1, 0, distance));
+
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		MoveLeft();
 		MoveRight();
+
+		// restrict player to playspace		
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 	}
 
 	void MoveLeft() {
 		if (Input.GetKey(KeyCode.LeftArrow)) {
 			transform.position -= new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+			// can also use transform.position -= Vector3.left * speed * Time.deltaTime;
 		}
 	}
 	
 	void MoveRight() {
 		if (Input.GetKey(KeyCode.RightArrow)) {
 			transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+			// can also use transform.position += Vector3.right * speed * Time.deltaTime;
 		}
 	}
+
 }
