59c8e4c2ca810475cb4ff69fccfea2e815da2180
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-06-17 18:08:14 -0400

    can successfully destroy enemies on hit. the enemies are a trigger and the collider is the bullet. Need to destroy both

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Destroy(this.gameObject);
+	}
+}
