23ee829931103d85b637856ad23c0678058372f3
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-07-08 16:44:55 -0400

    adding the Phase screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public int score = 0;
+	private int kills = 0;
+	public int phase = 1;
+	public bool phaseActive = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	public int phase; 
+	public GameObject GameManager;
+	private bool showPhase;
+
+
+	void Start () {
+		phase = GameManager.GetComponent<GameManager>().phase;
+		showPhase = GameManager.GetComponent<GameManager>().phaseActive;
+	}
+
+	void startTimer () {
+		if 
+	}
+
+}
