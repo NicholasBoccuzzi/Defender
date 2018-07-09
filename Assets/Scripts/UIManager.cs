c0810b1bf7e0265b63003279a51d9a79112c6970
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-07-08 22:38:45 -0400

    adding the functions necessary to make the phase display

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,15 +7,31 @@ public class UIManager : MonoBehaviour {
 	public int phase; 
 	public GameObject GameManager;
 	private bool showPhase;
-
+	private float timer;
+	private bool timerStarted;
 
 	void Start () {
 		phase = GameManager.GetComponent<GameManager>().phase;
 		showPhase = GameManager.GetComponent<GameManager>().phaseActive;
 	}
 
+	void Update () {
+		if (showPhase && timerStarted) {
+			timer -= Time.deltaTime;
+			if	(timer <= 2.5f) {
+
+			}
+			if (timer <= 0) {
+
+			}
+		}
+	}
+
 	void startTimer () {
-		if 
+		if (showPhase && !timerStarted) {
+			timer = 5.0f;
+			timerStarted = true;
+		}
 	}
 
 }
