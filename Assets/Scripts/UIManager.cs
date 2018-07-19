29ca2b870fef78600116b29d39d8d70307ce7af6
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-07-18 23:18:22 -0400

    added in a beginning phase sound effect

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,25 +6,32 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour {
 	public int phase; 
 	public GameObject GameManager;
+	private AudioSource uiAudio;
+	public AudioClip[] uiAudioClips;
 	private bool showPhase;
 	private float timer;
 	private bool timerStarted = false;
 	public Text text;
 	private Color color;
+	private bool played = false;
 
 	void Start () {
 		timer = 10.0f;
+		uiAudio = this.GetComponent<AudioSource>();
 		phase = GameManager.GetComponent<GameManager>().phase;
 		showPhase = GameManager.GetComponent<GameManager>().phaseActive;
 		text = GetComponent<Text>();
 		color = text.color;
-
 		startTimer();
 	}
 
 	void Update () {
 		text.text = "Phase " + phase.ToString();
 		if (showPhase && timerStarted) {
+			if (uiAudio && !played && timer <= 2.75) {
+				uiAudio.PlayOneShot(uiAudioClips[0]);
+				played = true;
+			}
 			timer -= Time.deltaTime;
 			if (timer <= 3.0f) {
 				color.a = (10 * timer/3.0f);
